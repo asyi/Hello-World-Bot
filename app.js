2fae6bde2db2029ac2bd6cc07f86887fffdf85c9
Althea Yi <altheaserena.yi@gmail.com>
2016-10-06 21:32:30 -0400

    Fixed code

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-// Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+//Add your requirements
+var restify = require('restify');
+var builder = require('botbuilder');
 
 var appId = process.env.MY_APP_ID || "Missing your app ID";
 var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
 
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot
+({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
+bot.add('/', new builder.SimpleDialog( function (session) {
+session.send('Hello World');
+}));
+
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
-   console.log('%s listening to %s', server.name, server.url); 
-});
-
-// Create chat bot
-var connector = new builder.ChatConnector
-({ appId: 'process.env.MY_APP_ID', appPassword: 'process.env.MY_APP_SECRET'});
-var bot = new builder.UniversalBot(connector);
-server.post('/api/messages', connector.listen());
-
-// Create bot dialogs
-bot.dialog('/', function (session) {
-    session.send("Hello World");
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3000, function () {
+console.log('%s listening to %s', server.name, server.url);
 });
 
 server.get('/', restify.serveStatic({
- directory: __dirname,
- default: '/index.html'
+    directory: __dirname,
+    default: '/index.html'
 }));
