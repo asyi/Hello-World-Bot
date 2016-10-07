3a8ac917293481c78fecc6fbac7dda73bdcf28ac
Althea Yi <altheaserena.yi@gmail.com>
2016-10-06 21:11:51 -0400

    Updated code to hide secrets etc.

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 // Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
 
-var restify = require('restify');
-var builder = require('botbuilder');
+var appId = process.env.MY_APP_ID || "Missing your app ID";
+var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function ()
+server.listen(process.env.PORT || 3000, function() 
 {
-  console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword'});
-var bot = new builder.UniversalBot(connector)
+({ appId: 'process.env.MY_APP_ID', appPassword: 'process.env.MY_APP_SECRET'});
+var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Create bot dialogs
 bot.dialog('/', function (session) {
-  session.send("Hello World");
+    session.send("Hello World");
 });
+
+server.get('/', restify.serveStatic({
+ directory: __dirname,
+ default: '/index.html'
+}));
