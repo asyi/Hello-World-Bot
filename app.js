7f63ace341ae1725752bd34281d261c133225d9e
Althea Yi <altheaserena.yi@gmail.com>
2016-10-07 19:00:29 -0400

    Added code to save/change name

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-/*-----------------------------------------------------------------------------
-A simple "Hello World" bot for the Microsoft Bot Framework.
------------------------------------------------------------------------------*/
-
+var builder = require('botbuilder');
 var restify = require('restify');
-var builder = require('../../core/');
 
 //=========================================================
 // Bot Setup
@@ -27,6 +23,37 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
